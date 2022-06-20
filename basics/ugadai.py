081f9b298b6f6ac2f2459de43423b1e9a768480b
Ljazz <elabdrasulov@gmail.com>
2022-06-20 20:29:11 +0600

    ugadai

--- basics/ugadai.py
+++ basics/ugadai.py
@@ -2,22 +2,24 @@ from random import randint
 
 num = randint(1, 10)
 
-name = input('Как тебя зовут?\n') 
-print(f'\n{name}, хочешь сыграть в игру "Угадай число"? (да / нет)')
-
-contin = input().lower()
 contin = 'да'
+name = input('Как тебя зовут?\n') 
 tries = 0
-while contin == 'да':
-    user_num = int(input('\nТвоя догадка: '))
-    tries += 1
-    if user_num < num:
-        print('\nЭто число больше!')
-    elif user_num > num:
-        print('\nЭто число меньше!')
-    else:
-        print('\nТы угадал!')
-        print(f'Количество попыток: {tries}')
-        contin = input(f'\n{name}, хочешь сыграть в игру "Угадай число"? (да / нет)\n').lower()
-        tries = 0
+contin = input(f'\n{name}, хочешь сыграть в игру "Угадай число"? (да / нет)\n').lower()
+
+while contin not in ('да', 'нет'):
+    contin = input('\nДа или нет?!\n').lower()
+if contin == 'да':
+    while contin == 'да':
+        user_num = int(input('\nТвоя догадка: '))
+        tries += 1
+        if user_num < num:
+            print('\nЭто число больше!')
+        elif user_num > num:
+            print('\nЭто число меньше!')
+        else:
+            print('\nТы угадал!')
+            print(f'Количество попыток: {tries}')
+            contin = input(f'\n{name}, хочешь сыграть в игру "Угадай число"? (да / нет)\n').lower()
+            tries = 0
 
